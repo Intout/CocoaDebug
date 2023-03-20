9d4631f7b68132163937ac169e966fe25e876d78
Mert Tecimen <merttecimen@yahoo.com.tr>
2023-03-20 15:19:06 +0300

    Edited package.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,6 @@
+// swift-tools-version:5.7.1
 import PackageDescription
 
-// swift-tools-version:5.7.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 let package = Package(
     name: "CocoaDebug",
     platforms: [
