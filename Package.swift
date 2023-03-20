f088c145cd1e6d2d008d59f5e5a1446defcfdab6
Mert Tecimen <merttecimen@yahoo.com.tr>
2023-03-20 15:08:26 +0300

    Added swift-tools-version.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,7 @@
 import PackageDescription
 
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 let package = Package(
     name: "CocoaDebug",
     platforms: [
