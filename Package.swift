27e960a9675c0a54dde789d549f4af7e7ba91187
Mert Tecimen <merttecimen@yahoo.com.tr>
2023-03-21 15:30:12 +0300

    Separated Objc code as module

--- Package.swift
+++ Package.swift
@@ -7,10 +7,56 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        .library(name: "CocoaDebug", targets: ["CocoaDebug"]),
+        .library(name: "CocoaDebug", targets: ["ObjcModule"]),
     ],
     dependencies: [],
     targets: [
-        .target(name: "CocoaDebug", dependencies: [], path: "CocoaDebug"),
+        .target(
+            name: "ObjcModule",
+               dependencies: [],
+               path: "Sources/ObjcSource")
+        /*
+        .target(
+            name: "MonitorObjc",
+            dependencies: ["LogsObjc"],
+            path: "Sources/ObjcSource/Monitor",
+            cSettings: [.headerSearchPath("include")]
+        ),
+        .target(
+            name: "CoreObjc",
+            dependencies: ["NetworkObjc"],
+            path: "Sources/ObjcSource/Core",
+            cSettings: [.headerSearchPath("include")]),
+        .target(
+            name: "LogsObjc",
+            dependencies: ["CategoriesObjc", "FishhookObjc"],
+            path: "Sources/ObjcSource/Logs",
+            cSettings: [.headerSearchPath("include")]),
+        .target(
+            name: "CategoriesObjc",
+            dependencies: [],
+            path: "Sources/ObjcSource/Categories",
+            cSettings: [.headerSearchPath("include")]),
+        .target(
+            name: "NetworkObjc",
+            dependencies: ["FishhookObjc", "CustomHTTPProtocolObjc", "CategoriesObjc"],
+            path: "Sources/ObjcSource/Network",
+            cSettings: [.headerSearchPath("include")]),
+        .target(
+            name: "FishhookObjc",
+            dependencies: [],
+            path: "Sources/ObjcSource/fishhook",
+            cSettings: [.headerSearchPath("include")]),
+        .target(
+            name: "CustomHTTPProtocolObjc",
+            dependencies: ["SwizzlingObjc"],
+            path: "Sources/ObjcSource/CustomHTTPProtocol",
+            cSettings: [.headerSearchPath("include")]),
+        .target(
+            name: "SwizzlingObjc",
+            dependencies: ["LogsObjc"],
+            path: "Sources/ObjcSource/Swizzling",
+            cSettings: [.headerSearchPath("include")]),
+         */
     ]
 )
